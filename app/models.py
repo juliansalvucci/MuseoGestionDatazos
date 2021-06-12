3c026fe32afeea83694b2b16f11fce63ef972139
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-06-12 20:06:22 -0300

    PuedeMigrar

--- app/models.py
+++ app/models.py
@@ -51,27 +51,18 @@ class Especialidad(models.Model):
         verbose_name=_('Estilo'),
         help_text=_('Estilo'),
         related_name= 'Estilo',
+        on_delete=models.SET_NULL,
         blank = False,
-        null = False
+        null = True
     )
     tecnica = models.ForeignKey(
         Tecnica,
         verbose_name=_('Tecnica'),
         help_text=_('Técnica'),
         related_name='Tecnica',
+        on_delete=models.SET_NULL,
         blank = False,
-        null = False
-    )
-
-class TipoRestauracion(models.Model):
-    descripcion = models.TextField(
-       _('Descripción del tipo de restauracion'),
-       help_text=_('Descripción'),
-    )
-    nombre = models.CharField(    
-        _('Nombre de tipo de restauración'),              
-        help_text=_('nombre tipo'), 
-        max_length=200,  
+        null = True
     )
 
 class Restaurador(models.Model):
@@ -95,29 +86,18 @@ class Restaurador(models.Model):
         help_text=_('Nombre restaurador'), 
         max_length=200, 
     )
-    telefono = models.PositiveIntegerField(
+    telefono = models.PositiveBigIntegerField(
         _('Teléfono'),
         help_text=_('Teléfono de restaurador'), 
-        max_length=200, 
     )
     especialidad = models.ForeignKey(
         Especialidad,
         verbose_name=_('Especialidad'),
         help_text=_('Especialidad derestaurador'),
         related_name='%(app_label)s_%(class)s_related',
+        on_delete=models.SET_NULL,
         blank = False,
-        null =False,
-    )
-
-class Estilo(models.Model):
-    descripcion = models.TextField(
-        _('descripción'),
-        help_text=_('Descripción de la obra')
-    )
-    nombre = models.CharField(    
-        _('Nombre del estilo'),              
-        help_text=_('Nombre del estilo aplicado'), 
-        max_length=200,  
+        null = True
     )
 
 class Restauracion(models.Model):
@@ -142,16 +122,18 @@ class Restauracion(models.Model):
         verbose_name=_('Restaurador'),
         help_text=_('restaurador que realizó la restauración'),
         related_name='%(app_label)s_%(class)s_related',
+        on_delete=models.SET_NULL,
         blank = False,
-        null = False
+        null = True
     )
     tipo_restauracion = models.ForeignKey(
         TipoRestauracion,
         verbose_name=_('Tipo de restauración'),
         help_text=_('Tipo de restauración aplicada'),
         related_name='%(app_label)s_%(class)s_related',
+        on_delete=models.SET_NULL,
         blank = False,
-        null = False
+        null = True
     )
 
 class Obra(models.Model):
@@ -193,121 +175,34 @@ class Obra(models.Model):
         verbose_name=_('Tecnica'),
         help_text=_('Tecnica empleada para la reparación'),
         related_name='%(app_label)s_%(class)s_related',
+        on_delete=models.SET_NULL,
         blank = False,
-        null = False
+        null = True
     )
     estilo = models.ForeignKey(
         Estilo,
         verbose_name=_('Estilo'),
         help_text=_('Estilo de reparación'),
         related_name='%(app_label)s_%(class)s_related',
+        on_delete=models.SET_NULL,
         blank = False,
-        null = False
+        null = True
     )
     restauración = models.ForeignKey(
         Restauracion,
         verbose_name=_('Restauración'),
         help_text=_('Restauraciones de obra'),
         related_name='%(app_label)s_%(class)s_related',
+        on_delete=models.SET_NULL,
         blank = True,
         null = True
     )
 
-class Restauracion(models.Model):
-    descripcion = models.TextField(
-        _('descripción'),
-        help_text=_('Descripción de la obra')
-    )
-    fecha_reingreso = models.DateField(
-        _('fecha de reingreso ingreso'),
-        help_text=_('fecha de primer ingreso de la obra')
-    )
-    fecha_salida = models.DateField(
-        _('fecha de salida'),
-        help_text=_('fecha de salida de la obra')
-    )
-    precio = models.FloatField(
-        _('Precio de la reparación'),
-        help_text=_('precio de la reparación')
-    )
-    restaurador = models.ForeignKey(
-        Restaurador,
-        verbose_name=_('Restaurador'),
-        help_text=_('restaurador que realizó la restauración'),
-        related_name='%(app_label)s_%(class)s_related',
-        blank = False,
-        null = False
-    )
-    tipo_restauracion = models.ForeignKey(
-        TipoRestauracion,
-        verbose_name=_('Tipo de restauración'),
-        help_text=_('Tipo de restauración aplicada'),
-        related_name='%(app_label)s_%(class)s_related',
-        blank = False,
-        null = False
-    )
 
-class Restaurador(models.Model):
-    apellido = models.CharField(    
-        _('Apellido'),              
-        help_text=_('Apellido restaurador'), 
-        max_length=200,  
-    )
-    nombre = models.CharField(    
-        _('Nombre'),              
-        help_text=_('Nombre restaurador'), 
-        max_length=200,  
-    )
-    domicilio = models.CharField(    
-        _('Domicilio'),              
-        help_text=_('Domicilio del restaurador'), 
-        max_length=200,  
-    )
-    email = models.EmailField(
-        _('E-mail'),
-        help_text=_('Nombre restaurador'), 
-        max_length=200, 
-    )
-    telefono = models.PositiveIntegerField(
-        _('Teléfono'),
-        help_text=_('Teléfono de restaurador'), 
-        max_length=200, 
-    )
-    especialidad = models.ForeignKey(
-        Especialidad,
-        verbose_name=_('Especialidad'),
-        help_text=_('Especialidad derestaurador'),
-        related_name='%(app_label)s_%(class)s_related',
-        blank = False,
-        null =False,
-    )
+
+
   
-class Especialidad(models.Model):
-    descripcion = models.TextField(
-      _('descripción'),
-      help_text=_('Descripción de la obra')
-    )
-    nombre = models.CharField(    
-        _('Nombre de Obra'),              
-        help_text=_('Nombre de la obra'), 
-        max_length=200,  
-    )
-    estilo = models.ForeignKey(
-        Estilo,
-        verbose_name=_('Estilo'),
-        help_text=_('Estilo'),
-        related_name= 'Estilo',
-        blank = False,
-        null = False
-    )
-    tecnica = models.ForeignKey(
-        Tecnica,
-        verbose_name=_('Tecnica'),
-        help_text=_('Técnica'),
-        related_name='Tecnica',
-        blank = False,
-        null = False
-    )
+
 
 
 
